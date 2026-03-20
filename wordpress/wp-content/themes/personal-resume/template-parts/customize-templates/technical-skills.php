2514800c6dd00e78f2058fdc258046ab81943295
lelouchjek77 <lelouch.jek77@gmail.com>
2026-03-20 15:34:48 +0800

    update technical skill

--- wordpress/wp-content/themes/personal-resume/template-parts/customize-templates/technical-skills.php
+++ wordpress/wp-content/themes/personal-resume/template-parts/customize-templates/technical-skills.php
@@ -149,7 +149,7 @@
                 <span>C#</span>
                 <span>VB.NET</span>
                 <span>Java (Struts)</span>
-                <span>Node.js (Basic)</span>
+                <span>Node.js</span>
             </div>
         </div>
 
@@ -159,6 +159,7 @@
                 <span>Laravel</span>
                 <span>React.js</span>
                 <span>Vue.js</span>
+                <span>Typescript</span>
                 <span>jQuery</span>
                 <span>WordPress</span>
                 <span>.NET Framework</span>
@@ -182,6 +183,7 @@
                 <span>Git</span>
                 <span>Github</span>
                 <span>Open AI</span>
+                <span>Docker</span>
                 <span>FileZilla</span>
                 <span>REST APIs</span>
                 <span>AWS</span>
