0b7298050334206422eb65e7e7ba78e432a64156
lelouchjek77 <lelouch.jek77@gmail.com>
2026-03-20 15:27:49 +0800

    Update info

--- wordpress/wp-content/themes/personal-resume/template-parts/customize-templates/technical-skills.php
+++ wordpress/wp-content/themes/personal-resume/template-parts/customize-templates/technical-skills.php
@@ -187,6 +187,8 @@
                 <span>FileZilla</span>
                 <span>REST APIs</span>
                 <span>AWS</span>
+                <span>Postman</span>
+                <span>VSCode</span>
                 <span>Linux Server Deployment</span>
             </div>
         </div>
